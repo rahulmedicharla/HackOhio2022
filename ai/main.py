8b1d02451a8bfe42b16d8a0cff43d75ea0c55de4
EthanYoung08 <young.3251@buckeyemail.osu.edu>
2022-10-08 15:41:36 -0400

    git ignore

--- ai/main.py
+++ ai/main.py
@@ -10,12 +10,12 @@ toggle_mic = False
 def get_speech_over_duration(dur):
 
     with sr.Microphone() as source:
-        audio_data = r.record(source, duration=dur)
         print("Listening...")
+        audio_data = r.record(source, duration=dur)
+        print("Interpreting...")
         audio = r.recognize_google(audio_data)
         #print(audio)
 
     return audio
 
 print(get_speech_over_duration(10))
-
